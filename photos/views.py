987e24f823a7ddaee73f6b9fbb4b49b76c356e6d
unknown <naeemislam13790@gmail.com>
2021-07-06 13:58:33 +0600

    'commited'

--- photos/views.py
+++ photos/views.py
@@ -23,24 +23,24 @@ def add(request):
     
     if request.method == 'POST':
         data = request.POST  
-        imag = request.FILES.get('im')
-        if data['category'] != 'none':
-            category = Catagory.objects.get(id = data['category'])
-        elif data['new_category'] != '':
-            category, created = Catagory.objects.get_or_create(name=data['new_category'])
-        else:
-            category = None
-        
-        photo = Photo.objects.create(photo = imag,catagory = category,description=data['description'])
-        
-        return redirect('index')
-
+        imag = request.FILES
         # "data = request.POST" by this code we can grab all data
         # "img = request.FILES" by this code we can grab all image
         # "data = request.POST.get('description')" by this code we can grab only description data nothing more
         # "img = request.FILES.get('image')" by this code we can grab only image's picture data nothing more
        
+        if data['category'] != 'none':      #if work when u select a category that already exixts
+            category = Catagory.objects.get(id = data['category'])        #by 'id=data['category'] we bring our exact category id
+        elif data['new_category'] != '':                                 # it works when u create a new category
+            category, created = Catagory.objects.get_or_create(name=data['new_category'])              # here created variable is default. 'get_or_create' will create a category and bring the category from database once time
+        else: 
+            category = None                                      #it works when u select neither existing category nor new category
         
+        photo = Photo.objects.create(photo = imag['im'],catagory = category,description=data['description'])
+        
+        return redirect('index')
+
+
 
     context={
 
