c195eea568b218bf00acc2adda366be964e80db2
unknown <naeemislam13790@gmail.com>
2021-08-08 00:34:52 +0600

    'commited'

--- image_gallery/settings.py
+++ image_gallery/settings.py
@@ -112,6 +112,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static', ]
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'collect_fill'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Default primary key field type
