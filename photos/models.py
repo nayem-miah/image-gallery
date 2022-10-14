0ebac95944b75a799904b43e429ff0a8c547f36d
nayem <naeemislam13790@gmail.com>
2022-10-15 00:21:27 +0600

    'commited'

--- photos/models.py
+++ photos/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from io import BytesIO
+from PIL import Image
+from django.core.files import File
+
+def compress(image):
+    im = Image.open(image)
+    im_io = BytesIO() 
+    im.save(im_io, 'JPEG', quality=10) 
+    new_image = File(im_io, name=image.name)
+    return new_image
 
 class Catagory(models.Model):
     name = models.CharField(max_length=25)
@@ -11,4 +21,12 @@ class Photo(models.Model):
     photo = models.ImageField(null=False, blank=False)
     def __str__(self):
         return self.description
+
+    def save(self, *args, **kwargs):
+                new_image = compress(self.photo)
+                self.photo = new_image
+                super().save(*args, **kwargs)
+
+
+
 
